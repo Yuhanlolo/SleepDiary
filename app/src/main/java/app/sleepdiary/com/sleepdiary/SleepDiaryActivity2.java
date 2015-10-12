1aeb94b8eb940ae3fde696abb6f9e574f96896cf
Yuhan Luo <ypl5142@yluo-imac.ist.psu.edu>
2015-10-12 12:50:58 -0400

    face selection added

--- app/src/main/java/app/sleepdiary/com/sleepdiary/SleepDiaryActivity2.java
+++ app/src/main/java/app/sleepdiary/com/sleepdiary/SleepDiaryActivity2.java
@@ -2,6 +2,7 @@ package app.sleepdiary.com.sleepdiary;
 
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.AdapterView;
 
@@ -14,6 +15,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.view.View;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -24,7 +27,7 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class SleepDiaryActivity2 extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener{
+public class SleepDiaryActivity2 extends ActionBarActivity implements OnSeekBarChangeListener{
 
     private Button Bbedtime;
     private Button Basleeptime;
@@ -36,13 +39,39 @@ public class SleepDiaryActivity2 extends ActionBarActivity implements SeekBar.On
     private int pHour;
     private int pMinute;
 
+//    private RadioGroup SQ;
+//    private RadioGroup AWQ;
+
+    private ImageView SQ1_g;
+    private ImageView SQ1;
+    private ImageView SQ2_g;
+    private ImageView SQ2;
+    private ImageView SQ3_g;
+    private ImageView SQ3;
+    private ImageView SQ4_g;
+    private ImageView SQ4;
+    private ImageView SQ5_g;
+    private ImageView SQ5;
+
+    private ImageView AWQ1_g;
+    private ImageView AWQ1;
+    private ImageView AWQ2_g;
+    private ImageView AWQ2;
+    private ImageView AWQ3_g;
+    private ImageView AWQ3;
+    private ImageView AWQ4_g;
+    private ImageView AWQ4;
+    private ImageView AWQ5_g;
+    private ImageView AWQ5;
+
+    int sq = 0;
+    int awq = 0;
+
     String bedtime = "";
     String asleeptime = "";
     String woketime = "";
     String outtime = "";
     int no_wake = -1;
-    int facesleep = 0;
-    int facewake =0;
 
 
     /** This integer will uniquely define the dialog to be used for displaying time picker.*/
@@ -94,16 +123,136 @@ public class SleepDiaryActivity2 extends ActionBarActivity implements SeekBar.On
 
         waket = (TextView)findViewById(R.id.d_wake);
 
+
+        SQ1_g = (ImageView)findViewById(R.id.SQquality1_gray);
+        SQ1 = (ImageView)findViewById(R.id.SQquality1);
+        SQ2_g = (ImageView)findViewById(R.id.SQquality2_gray);
+        SQ2 = (ImageView)findViewById(R.id.SQquality2);
+        SQ3_g = (ImageView)findViewById(R.id.SQquality3_gray);
+        SQ3 = (ImageView)findViewById(R.id.SQquality3);
+        SQ4_g = (ImageView)findViewById(R.id.SQquality4_gray);
+        SQ4 = (ImageView)findViewById(R.id.SQquality4);
+        SQ5_g = (ImageView)findViewById(R.id.SQquality5_gray);
+        SQ5 = (ImageView)findViewById(R.id.SQquality5);
+
+
+        AWQ1_g = (ImageView)findViewById(R.id.AWQquality1_gray);
+        AWQ1 = (ImageView)findViewById(R.id.AWQquality1);
+        AWQ2_g = (ImageView)findViewById(R.id.AWQquality2_gray);
+        AWQ2 = (ImageView)findViewById(R.id.AWQquality2);
+        AWQ3_g = (ImageView)findViewById(R.id.AWQquality3_gray);
+        AWQ3 = (ImageView)findViewById(R.id.AWQquality3);
+        AWQ4_g = (ImageView)findViewById(R.id.AWQquality4_gray);
+        AWQ4 = (ImageView)findViewById(R.id.AWQquality4);
+        AWQ5_g = (ImageView)findViewById(R.id.AWQquality5_gray);
+        AWQ5 = (ImageView)findViewById(R.id.AWQquality5);
+
     }
+
+
+
 
     public void button_SD2dOnClick(View view)
     {
-        if(view.getId() == R.id.save_s2)
+        if(view.getId() == SQ1_g.getId()){
+            sq =1;
+            SQ1.setVisibility(View.VISIBLE);
+            SQ2.setVisibility(View.INVISIBLE);
+            SQ3.setVisibility(View.INVISIBLE);
+            SQ4.setVisibility(View.INVISIBLE);
+            SQ5.setVisibility(View.INVISIBLE);
+
+        }
+        else if(view.getId() == SQ2_g.getId()){
+            sq =2;
+            SQ2.setVisibility(View.VISIBLE);
+            SQ1.setVisibility(View.INVISIBLE);
+            SQ3.setVisibility(View.INVISIBLE);
+            SQ4.setVisibility(View.INVISIBLE);
+            SQ5.setVisibility(View.INVISIBLE);
+
+        }
+
+        else if(view.getId() == SQ3_g.getId()){
+            sq =3;
+            SQ3.setVisibility(View.VISIBLE);
+            SQ2.setVisibility(View.INVISIBLE);
+            SQ1.setVisibility(View.INVISIBLE);
+            SQ4.setVisibility(View.INVISIBLE);
+            SQ5.setVisibility(View.INVISIBLE);
+
+        }
+        else if(view.getId() == SQ4_g.getId()){
+            sq =4;
+            SQ4.setVisibility(View.VISIBLE);
+            SQ2.setVisibility(View.INVISIBLE);
+            SQ3.setVisibility(View.INVISIBLE);
+            SQ1.setVisibility(View.INVISIBLE);
+            SQ5.setVisibility(View.INVISIBLE);
+
+        }
+        else if(view.getId() == SQ5_g.getId()){
+            sq =5;
+            SQ5.setVisibility(View.VISIBLE);
+            SQ2.setVisibility(View.INVISIBLE);
+            SQ3.setVisibility(View.INVISIBLE);
+            SQ4.setVisibility(View.INVISIBLE);
+            SQ1.setVisibility(View.INVISIBLE);
+
+        }
+
+        else if(view.getId() == AWQ1_g.getId())
+            {
+                awq = 1;
+                AWQ1.setVisibility(View.VISIBLE);
+                AWQ2.setVisibility(View.INVISIBLE);
+                AWQ3.setVisibility(View.INVISIBLE);
+                AWQ4.setVisibility(View.INVISIBLE);
+                AWQ5.setVisibility(View.INVISIBLE);
+            }
+            else if(view.getId() == AWQ2_g.getId())
+            {
+                awq = 2;
+                AWQ2.setVisibility(View.VISIBLE);
+                AWQ1.setVisibility(View.INVISIBLE);
+                AWQ3.setVisibility(View.INVISIBLE);
+                AWQ4.setVisibility(View.INVISIBLE);
+                AWQ5.setVisibility(View.INVISIBLE);
+            }
+            else if(view.getId() == AWQ3_g.getId())
+            {
+                awq = 3;
+                AWQ3.setVisibility(View.VISIBLE);
+                AWQ2.setVisibility(View.INVISIBLE);
+                AWQ1.setVisibility(View.INVISIBLE);
+                AWQ4.setVisibility(View.INVISIBLE);
+                AWQ5.setVisibility(View.INVISIBLE);
+            }
+            else if(view.getId() == AWQ4_g.getId())
+            {
+                awq = 4;
+                AWQ4.setVisibility(View.VISIBLE);
+                AWQ2.setVisibility(View.INVISIBLE);
+                AWQ3.setVisibility(View.INVISIBLE);
+                AWQ1.setVisibility(View.INVISIBLE);
+                AWQ5.setVisibility(View.INVISIBLE);
+            }
+            else if(view.getId() == AWQ5_g.getId())
+            {
+                awq = 5;
+                AWQ5.setVisibility(View.VISIBLE);
+                AWQ2.setVisibility(View.INVISIBLE);
+                AWQ3.setVisibility(View.INVISIBLE);
+                AWQ4.setVisibility(View.INVISIBLE);
+                AWQ1.setVisibility(View.INVISIBLE);
+            }
+
+        else if(view.getId() == R.id.save_s2)
         {
 
 
 
-            if(bedtime.isEmpty()||asleeptime.isEmpty()||woketime.isEmpty()||outtime.isEmpty()||no_wake == -1)
+            if(bedtime.isEmpty()||asleeptime.isEmpty()||woketime.isEmpty()||outtime.isEmpty()||no_wake == -1||sq==0||awq==0)
             {
                 //popup msg
                 Toast errormsg = Toast.makeText(SleepDiaryActivity2.this,"Please finish all the questions!", Toast.LENGTH_SHORT);
@@ -111,26 +260,11 @@ public class SleepDiaryActivity2 extends ActionBarActivity implements SeekBar.On
 
             }
 
-            else
+            else if(view.getId() == R.id.cancel_s2)
             {
-
-//                SleepdiaryInfo s = new SleepdiaryInfo();
-//                s.setNo_coffee(Integer.parseInt(no_coffee));
-//                s.setNo_coffee(no_coffee);
-//                s.setNo_wine(no_wine);
-//
-//                s.setNo_smoke(no_smoke);
-//                s.setNo_naptime(no_naptime)
-                //               s.setSleepdurationday(sleepduration);
-//                s.setPilltime(pilltime);
-//                s.setPillname(pillname);
-//                sleephelper.insertColumn(s);
-
                 Intent i = new Intent(SleepDiaryActivity2.this,SleepDiaryActivity3.class);
-
                 SleepDiaryActivity2.this.startActivity(i);
-//                Toast msg = Toast.makeText(SleepDiaryActivity.this,"Finished this page!", Toast.LENGTH_SHORT);
-//                msg.show();
+
             }
         }
 
@@ -287,6 +421,116 @@ public class SleepDiaryActivity2 extends ActionBarActivity implements SeekBar.On
     public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
+
+
+//    public void onCheckedChanged(RadioGroup group, int checkedId) {
+//        if(group == SQ)
+//        {
+//            if(checkedId == sq1)
+//            {
+//                sq =1;
+//                SQ1.setVisibility(View.VISIBLE);
+//                SQ2.setVisibility(View.INVISIBLE);
+//                SQ3.setVisibility(View.INVISIBLE);
+//                SQ4.setVisibility(View.INVISIBLE);
+//                SQ5.setVisibility(View.INVISIBLE);
+//               Toast msg = Toast.makeText(SleepDiaryActivity2.this,"1", Toast.LENGTH_SHORT);
+//               msg.show();
+//            }
+//            else if (checkedId == sq2)
+//            {
+//                sq = 2;
+//                SQ2.setVisibility(View.VISIBLE);
+//                SQ1.setVisibility(View.INVISIBLE);
+//                SQ3.setVisibility(View.INVISIBLE);
+//                SQ4.setVisibility(View.INVISIBLE);
+//                SQ5.setVisibility(View.INVISIBLE);
+//                Toast msg = Toast.makeText(SleepDiaryActivity2.this,"2", Toast.LENGTH_SHORT);
+//                msg.show();
+//            }
+//            else if (checkedId == sq3)
+//            {
+//                sq = 3;
+//                SQ3.setVisibility(View.VISIBLE);
+//                SQ2.setVisibility(View.INVISIBLE);
+//                SQ1.setVisibility(View.INVISIBLE);
+//                SQ4.setVisibility(View.INVISIBLE);
+//                SQ5.setVisibility(View.INVISIBLE);
+//                Toast msg = Toast.makeText(SleepDiaryActivity2.this,"3", Toast.LENGTH_SHORT);
+//                msg.show();
+//            }
+//            else if (checkedId == sq4)
+//            {
+//                sq = 4;
+//                SQ4.setVisibility(View.VISIBLE);
+//                SQ2.setVisibility(View.INVISIBLE);
+//                SQ3.setVisibility(View.INVISIBLE);
+//                SQ1.setVisibility(View.INVISIBLE);
+//                SQ5.setVisibility(View.INVISIBLE);
+//                Toast msg = Toast.makeText(SleepDiaryActivity2.this,"4", Toast.LENGTH_SHORT);
+//                msg.show();
+//            }
+//            else if (checkedId == sq5)
+//            {
+//                sq = 5;
+//                SQ5.setVisibility(View.VISIBLE);
+//                SQ2.setVisibility(View.INVISIBLE);
+//                SQ3.setVisibility(View.INVISIBLE);
+//                SQ4.setVisibility(View.INVISIBLE);
+//                SQ1.setVisibility(View.INVISIBLE);
+//                Toast msg = Toast.makeText(SleepDiaryActivity2.this,"5", Toast.LENGTH_SHORT);
+//                msg.show();
+//            }
+//        }
+//        if(group == AWQ)
+//        {
+//            if(checkedId == awq1)
+//            {
+//                awq = 1;
+//                AWQ1.setVisibility(View.VISIBLE);
+//                AWQ2.setVisibility(View.INVISIBLE);
+//                AWQ3.setVisibility(View.INVISIBLE);
+//                AWQ4.setVisibility(View.INVISIBLE);
+//                AWQ5.setVisibility(View.INVISIBLE);
+//            }
+//            else if(checkedId == awq2)
+//            {
+//                awq = 2;
+//                AWQ2.setVisibility(View.VISIBLE);
+//                AWQ1.setVisibility(View.INVISIBLE);
+//                AWQ3.setVisibility(View.INVISIBLE);
+//                AWQ4.setVisibility(View.INVISIBLE);
+//                AWQ5.setVisibility(View.INVISIBLE);
+//            }
+//            else if(checkedId == awq3)
+//            {
+//                awq = 3;
+//                AWQ3.setVisibility(View.VISIBLE);
+//                AWQ2.setVisibility(View.INVISIBLE);
+//                AWQ1.setVisibility(View.INVISIBLE);
+//                AWQ4.setVisibility(View.INVISIBLE);
+//                AWQ5.setVisibility(View.INVISIBLE);
+//            }
+//            else if(checkedId == awq4)
+//            {
+//                awq = 4;
+//                AWQ4.setVisibility(View.VISIBLE);
+//                AWQ2.setVisibility(View.INVISIBLE);
+//                AWQ3.setVisibility(View.INVISIBLE);
+//                AWQ1.setVisibility(View.INVISIBLE);
+//                AWQ5.setVisibility(View.INVISIBLE);
+//            }
+//            else if(checkedId == awq5)
+//            {
+//                awq = 5;
+//                AWQ5.setVisibility(View.VISIBLE);
+//                AWQ2.setVisibility(View.INVISIBLE);
+//                AWQ3.setVisibility(View.INVISIBLE);
+//                AWQ4.setVisibility(View.INVISIBLE);
+//                AWQ1.setVisibility(View.INVISIBLE);
+//            }
+//        }
+//    }
 }
 
 
